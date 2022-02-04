af4dc6392693a228981e98e0d67fe30b2aa1323f
Lennart <lennarteichhorn@googlemail.com>
2022-02-04 10:18:01 +0000

    Formatted all files

--- components/GlobalStyles.tsx
+++ components/GlobalStyles.tsx
@@ -1,23 +1,24 @@
 const GlobalStyles = () => {
-    return <style jsx global>{`
-    html,
-body {
-  padding: 0;
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-}
+  return (
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans,
+          Droid Sans, Helvetica Neue, sans-serif;
+      }
 
-a {
-  color: inherit;
-  text-decoration: none;
-}
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
 
-* {
-  box-sizing: border-box;
-}
-
+      * {
+        box-sizing: border-box;
+      }
     `}</style>
+  )
 }
 
 export default GlobalStyles
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import type { AppProps } from 'next/app'
-import GlobalStyles from '../components/GlobalStyles'
+import type { AppProps } from "next/app"
+import GlobalStyles from "../components/GlobalStyles"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <><GlobalStyles/><Component {...pageProps} /></>
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
